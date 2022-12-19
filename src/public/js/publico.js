ad5c5a77e9fce6cb3f58dac95c46a4639182cb8c
JavierHdzO <fco_hernandez1705@hotmail.com>
2022-12-19 12:07:20 -0600

    121922 F [ADD]: sockets Public and Desktop

--- src/public/js/publico.js
+++ src/public/js/publico.js
@@ -1,3 +1,24 @@
 
+const lblTickets = document.querySelectorAll('.ticket');
+const lblDesktop = document.querySelectorAll('.desktop');
 
-console.log('Público HTML')
+const socket = io();
+
+socket.on('connect', ()=>{
+    console.log('connected');
+});
+
+socket.on('disconnect', ()=>{
+    console.log('disconnected');
+});
+
+socket.on('current-status', ( payload ) => {
+    
+
+    for (let i = 0; i < lblTickets.length; i++) {
+        lblTickets[i].textContent = `Ticket ${ payload[i].number || "X" }`;
+        lblDesktop[i].textContent = `Desktop ${payload[i].desktop.desktop}` || "Desktop X";
+        
+    }
+
+});
--- src/sockets/controller.js
+++ src/sockets/controller.js
@@ -5,19 +5,51 @@ import TicketControl from "../models/ticket-control.js";
 const socketController = async(socket) => {
     /** Listening */
     await TicketControl.init();
+    console.log(TicketControl.getTickets);
 
-
-    socket.emit('last-ticket', TicketControl.lastTicket);
-
+    
     socket.on("next-ticket", async( payload, callback )=>{
         const next = await TicketControl.next();
         callback(next);
     });
+    
+    
+    socket.on("ckeckin-ticket", async( payload, callback ) => {
 
+        const { desktop } = payload;
+        if( !desktop ){
+            return callback({
+                ok: false,
+                msg:"Desktop is required"
+            });
+        }
+
+        const ticket = await TicketControl.checkInTicket( payload );
+
+        socket.broadcast.emit('current-status', TicketControl.lastFourTickets);
+        socket.emit('tickets-pending', TicketControl.ticketsLenght);
+        socket.broadcast.emit('tickets-pending', TicketControl.ticketsLenght);
+
+        
+        if( !ticket ){
+            return  callback({
+                ok:false,
+                msg:"There are not tickets pending"
+            });
+        }
+
+        callback( {
+            ok:true,
+            data:{
+                ticket
+            }
+        } );
+    });
 
     /** Emit */
-
-    socket.emit();
+    socket.emit('last-ticket', TicketControl.lastTicket);
+    socket.emit('current-status', TicketControl.lastFourTickets);
+    socket.emit('tickets-pending', TicketControl.ticketsLenght);
 }
 
 export default socketController;
